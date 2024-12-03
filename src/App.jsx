602aa4ade16d7cc5ad83521d7db43340f64355e3
Maniraj726 <manikandanraja726@gmail.com>
2024-12-03 11:50:57 +0530

    commit message

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { CarouselDemo } from "./components/demo/CarouselDemo"
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <div className="px-20" >
+      <Button>Hello</Button>
+     <CarouselDemo />
+    </div>
+  )
+}
+
+export default App
